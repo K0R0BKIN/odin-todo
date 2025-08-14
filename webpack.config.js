670af00ec78a1954203e6757f949225e858824a7
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2025-08-14 13:50:37 +0300

    Add HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin(),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
