efeaf56e8fa7a5de4a595e34392f0a37a17e35a9
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2025-08-14 14:09:32 +0300

    Format

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # odin-todo
+
 Practice project.
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,14 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devtool: 'eval-cheap-source-map',
-  plugins: [
-    new HtmlWebpackPlugin(),
-  ],
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "eval-cheap-source-map",
+  plugins: [new HtmlWebpackPlugin()],
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
 };
