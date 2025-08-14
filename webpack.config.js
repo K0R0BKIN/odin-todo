783f12b35cb2d05add3b94f533406251080a7477
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2025-08-14 14:04:59 +0300

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'eval-cheap-source-map',
   plugins: [
     new HtmlWebpackPlugin(),
   ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
